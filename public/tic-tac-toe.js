dbe8f7059cc0f8110d88415daa53a3d75cb8b2ec
Eric Wong <contact.eric.wong@gmail.com>
2022-09-24 09:07:27 -0700

    move index.html to root and fix routes

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -72,8 +72,8 @@ function gameEvent(event) {
     if (target.dataset["row"] && target.dataset["col"]) {
         const row = target.dataset["row"];
         const col = target.dataset["col"];
-        const xImage = "<img src='./player-x.png' alt='X' width='150' />";
-        const oImage = "<img src='./player-o.png' alt='O' width='150' />";
+        const xImage = "<img src='./public/player-x.png' alt='X' width='150' />";
+        const oImage = "<img src='./public/player-o.png' alt='O' width='150' />";
         
         if (!marked[[row, col]]) {
             // mark row and col as filled
